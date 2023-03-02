e2d83ead00d14bbb63b92cdbc821d7d5a2617502
marcopisani002 <pisanimarco34@gmail.com>
2023-03-02 11:23:57 +0100

    create files

---
+++ database/migrations/2023_03_02_101053_create_restaurants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('cover_img');
+            $table->integer('phone_number');
+            $table->integer('vat');
+            $table->string('address');
+            //relazione One to One con users
+            $table->unsignedBigInteger("user_id");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
