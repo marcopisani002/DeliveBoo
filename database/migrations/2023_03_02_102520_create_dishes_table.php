78fa6b09ed3ef03998b3f395b17d95b714026951
marcopisani002 <pisanimarco34@gmail.com>
2023-03-02 11:34:04 +0100

    add tables

--- database/migrations/2023_03_02_102520_create_dishes_table.php
+++ database/migrations/2023_03_02_102520_create_dishes_table.php
@@ -11,16 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('restaurants', function (Blueprint $table) {
+        Schema::create('dishes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('cover_img');
-            $table->integer('phone_number');
-            $table->integer('vat');
-            $table->string('address');
-            //relazione One to One con users
-            // $table->unsignedBigInteger("user_id");
-            // $table->foreign("user_id")->references("id")->on("user");
+            $table->text('ingredients');
+            $table->text('description');
+            $table->float('price');
+            $table->boolean('hide')->default(true);
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('restaurants');
+        Schema::dropIfExists('dishes');
     }
 };
--- database/migrations/2023_03_02_102914_create_types_table.php
+++ database/migrations/2023_03_02_102914_create_types_table.php
@@ -11,16 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('restaurants', function (Blueprint $table) {
+        Schema::create('types', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('cover_img');
-            $table->integer('phone_number');
-            $table->integer('vat');
-            $table->string('address');
-            //relazione One to One con users
-            // $table->unsignedBigInteger("user_id");
-            // $table->foreign("user_id")->references("id")->on("user");
             $table->timestamps();
         });
     }
@@ -30,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('restaurants');
+        Schema::dropIfExists('types');
     }
 };
