1e39db20b915e1e7581b379e4f3671205009dc40
marcopisani002 <pisanimarco34@gmail.com>
2023-03-02 12:34:19 +0100

    fix bug on login session

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,7 +63,7 @@
                         @else
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
+                               <strong> {{ Auth::user()->name }}  {{ Auth::user()->surname }}</strong>
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
