0b3298036bd22e992137ba64f3831db2ae42aaaa
marcopisani002 <pisanimarco34@gmail.com>
2023-03-06 11:44:55 +0100

    kku

--- database/seeders/DishTableSeeder.php
+++ database/seeders/DishTableSeeder.php
@@ -17,10 +17,10 @@ class DishTableSeeder extends Seeder
     public function run(Faker $faker) {
         $restaurants = Restaurant::all();
 
-        for ($i = 0; $i < 20; $i++) {
+        for ($i = 0; $i < 10; $i++) {
             $dish = Dish::create([
                 "name" => $faker->realText(),
-                "cover_img" => "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Ffake&psig=AOvVaw1R6I2ndtPZoAGipwf114Ir&ust=1677859908776000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKDz27DRvf0CFQAAAAAdAAAAABAD",
+                "cover_img" => "/dish/hU8vhk51JTqbp0qcypiW6qnmXaw7InmzQLEtr8ho.jpg",
                 "ingredients" => $faker->realText(),
                 "description" => $faker->sentence(),
                 "price" => rand(5, 50),
