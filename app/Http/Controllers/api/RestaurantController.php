9a1c9e7b47649afe8f99e280123b3354394f516e
marcopisani002 <pisanimarco34@gmail.com>
2023-03-09 15:00:25 +0100

    add api

---
+++ app/Http/Controllers/api/RestaurantController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $restaurants = Restaurant::all();
+
+	return response()->json($restaurants);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Restaurant $restaurant)
+    {
+        $restaurant->all();
+        return response()->json($restaurant);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\api\DishController as ApiDishController;
+use App\Http\Controllers\api\RestaurantController as ApiRestaurantController;
+use App\Http\Controllers\DishController;
+use App\Http\Controllers\RestaurantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +21,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/restaurants", [ApiRestaurantController::class, "index"]);
+
+// sarà raggiungibile tramite /api/posts/4
+Route::get("/restaurants/{restaurant}", [ApiRestaurantController::class, "show"]);
